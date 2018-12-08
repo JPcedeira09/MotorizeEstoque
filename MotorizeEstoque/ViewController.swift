696147aa586f8710c5c34e4158e062cf0b105227
Joao Paulo <joao.paulo@hvarconsulting.com.br>
2018-12-08 15:16:26 -0200

    Initial Commit

---
+++ MotorizeEstoque/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MotorizeEstoque
+//
+//  Created by Joao Paulo on 08/12/18.
+//  Copyright © 2018 Joao Paulo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
